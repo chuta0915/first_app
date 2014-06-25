669a615f2567fa13353df3a2989efa3b0edba78c
T.Ishii <tomoyasu0620@gmail.com>
2014-06-25 16:30:08 +0900

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  describe "Home page" do
+
+    it " should have the content 'First App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('First App')
+    end
+    it " should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Home')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+    it " should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Help')
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+    it " should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('About Us')
+    end
+  end
+
+#  describe "GET /static_pages" do
+#    it "works! (now write some real specs)" do
+#      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+#      get static_pages_index_path
+#      response.status.should be(200)
+#    end
+#  end
+end
